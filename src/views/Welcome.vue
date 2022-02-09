e7a642b97214e483f31aace6bca470807c531429
hoot.hoot <arkadij@gmail.com>
2022-02-09 14:26:40 +0100

    Version 0.0.1 - Boiler plated Start

---
+++ src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="welcome container">
+      <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .welcome{
+        text-align: center;
+    }
+</style>
