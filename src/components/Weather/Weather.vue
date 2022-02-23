eda17da2588db1b4836e6ba6ede458b740b79d8c
hoot.hoot <arkadij@gmail.com>
2022-02-23 12:22:57 +0100

    Weather API - Needs to be migrated to Composition API later

--- src/components/Weather/Weather.vue
+++ src/components/Weather/Weather.vue
@@ -1,16 +1,6 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
-    <main>
-      <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
-          placeholder="Search..."
-          v-model="query"
-          @keypress="fetchWeather"
-        />
-      </div>
-
+  
+    <div @vnode-mounted="fetchWeather">
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
@@ -18,34 +8,35 @@
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-          <div class="weather">{{ weather.weather[0].main }}</div>
+          <div class="temp"> {{ Math.round(weather.main.temp) }}°c </div>
+          <div class="weather"> {{ weather.weather[0].main }} </div>
+          <div v-if="weather.wind.speed < 1" class="wind"> {{ weather.wind.speed }} Calm </div>
+          <div v-if="weather.wind.speed > 1 && weather.wind.speed < 3" class="wind"> {{ weather.wind.speed }} Light Air </div>
+          <div v-if="weather.wind.speed > 3" class="wind"> {{ weather.wind.speed }} PLAY THE CONDITION! </div>    
         </div>
       </div>
-    </main>
-  </div>
+    </div>
+
 </template>
 
 <script>
 export default {
-  name: 'app',
   data () {
     return {
       api_key: 'c2ac0d3bbf9a640778032fa63629abc0',
       url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: '',
+      query: 'Berlin',
       weather: {}
     }
   },
   methods: {
-    fetchWeather (e) {
-      if (e.key == "Enter") {
+    fetchWeather () {
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
-      }
     },
+    
     setResults (results) {
       this.weather = results;
     },
